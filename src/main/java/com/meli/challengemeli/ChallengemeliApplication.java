53c253032bbef8ce312ea660126f030b046f57ac
dfmoreno <dfmoreno@asesoftware.com>
2021-02-26 18:50:58 -0500

    Base Proyecto

---
+++ src/main/java/com/meli/challengemeli/ChallengemeliApplication.java
@@ -0,0 +1,13 @@
+package com.meli.challengemeli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengemeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengemeliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meli/challengemeli/ChallengemeliApplicationTests.java
@@ -0,0 +1,16 @@
+package com.meli.challengemeli;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengemeliApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
